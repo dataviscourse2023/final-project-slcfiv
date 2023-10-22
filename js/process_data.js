cb05b845d2f139a3ce915b469fbfad07971c8e53
Arleth Salinas <arleth@Arleths-MBP.lan>
2023-10-22 15:50:35 -0600

    tiny menu trying to then add barchart

--- js/process_data.js
+++ js/process_data.js
@@ -8,200 +8,212 @@
 // put them at the bottom of the process_data function
 
 // used for deriving data from things
-let derivative_function = function(x){ return x }
+let derivative_function = function (x) {
+  return x;
+};
 
 class ProcessData {
-    constructor(json) {
-        this.restaurants = [];
-        this.done_processing = false;
-        this.data = json.data;
+  constructor(json) {
+    this.restaurants = [];
+    this.done_processing = false;
+    this.data = json.data;
+  }
+
+  process_data() {
+    console.log("Processing data in function!");
+    let first_row = this.data[0];
+    let restaurant = new Restaurant(
+      first_row[0],
+      first_row[2],
+      first_row[3],
+      first_row[4]
+    );
+    let inspection = new Inspection(first_row[1]);
+
+    // go through all of the data and put it into our lists
+    for (let i = 0; i < this.data.length; i++) {
+      let row = this.data[i];
+
+      if (restaurant.id !== row[0]) {
+        // check if we are done with the current restaurant
+
+        // add the current inspection to the restaurant
+        restaurant.add_inspection(inspection);
+
+        // calculate the statistics of the previous restaurant and add to list
+        restaurant.calculate_statistics();
+        this.restaurants.push(restaurant);
+
+        // make a new restaurant and inspection
+        restaurant = new Restaurant(row[0], row[2], row[3], row[4]);
+        inspection = new Inspection(row[1]);
+      } else if (inspection.date !== row[1]) {
+        // check if we are done with the current inspection
+
+        restaurant.add_inspection(inspection);
+        inspection = new Inspection(row[1]);
+      }
+
+      // add the current inspection
+      let code = row[5];
+      if (code !== "N/A") {
+        let code_period = code.indexOf(".");
+        inspection.add_violation(
+          code.substring(0, code_period),
+          code.substring(code_period + 1),
+          row[7],
+          row[8],
+          row[9],
+          row[10],
+          row[11]
+        );
+      }
     }
 
-    process_data(){ 
-        console.log("Processing data in function!")
-        let first_row = this.data[0]
-        let restaurant = new Restaurant(first_row[0], first_row[2], first_row[3], first_row[4])
-        let inspection = new Inspection(first_row[1])
-    
-        // go through all of the data and put it into our lists
-        for(let i = 0; i < this.data.length; i++){
-            let row = this.data[i]
-    
-            if (restaurant.id !== row[0] ){
-            // check if we are done with the current restaurant
-    
-                // add the current inspection to the restaurant
-                restaurant.add_inspection(inspection)
-    
-                // calculate the statistics of the previous restaurant and add to list
-                restaurant.calculate_statistics()
-                this.restaurants.push(restaurant)
-    
-                // make a new restaurant and inspection
-                restaurant = new Restaurant(row[0], row[2], row[3], row[4])
-                inspection = new Inspection(row[1])
-            }
-            else if (inspection.date !== row[1] ){
-            // check if we are done with the current inspection
-    
-                restaurant.add_inspection(inspection)
-                inspection = new Inspection(row[1])
-            }
-    
-            // add the current inspection
-            let code = row[5]
-            if(code !== "N/A" ){
-                let code_period = code.indexOf(".")
-                inspection.add_violation(
-                    code.substring(0, code_period),
-                    code.substring(code_period+1),
-                    row[7], row[8], row[9], row[10], row[11])
-            }
-        }
-    
-        // add the rest of the data
-        restaurant.add_inspection(inspection)
-        restaurant.calculate_statistics()
-        this.restaurants.push(restaurant)
-    
-        this.done_processing = true
-        console.log("Done processing data!")
-        console.log(this.restaurants[0])
+    // add the rest of the data
+    restaurant.add_inspection(inspection);
+    restaurant.calculate_statistics();
+    this.restaurants.push(restaurant);
+
+    this.done_processing = true;
+    console.log("Done processing data!");
+    console.log(this.restaurants[0]);
+  }
+
+  /*
+   * Overview: Input a list of functions and some search parameters. It will return a list
+   *           filtered and (optionally) sorted based on the criteria. If you are searching
+   *           if the string contains a certain character, the sorting will favor restaurants
+   *           where that search term occurs earlier in the string. For example, if you are
+   *           searching for restaurants whose names contain "A", the first things in the list
+   *           will be restaurants whose name starts with "A", then has a second letter "A", etc.
+   *           Note that the list that is returned is a copy of the list. The original is retained.
+   *
+   * Params:
+   * restaurant_list: the list that you are filtering down
+   * by: A string describing what you would like to filter by. Options are:
+   *      - "address": does the address contain the given string
+   *      - "nameStartsWith": does the restaurant name start with the given string
+   *      - "nameContains": does the restaurant name contain the given string
+   *      - "town": does the town name contain the given string
+   * what: the string of the search query
+   * sort: whether or not the data should also be sorted by the thing that you are searching by
+   *
+   */
+  filtered_by(restaurant_list, by, what, sort = true) {
+    let filter_function = null;
+    switch (by) {
+      case "address":
+        filter_function = (x) => x.address;
+        break;
+      case "nameStartsWith":
+      case "nameContains":
+        filter_function = (x) => x.name;
+        break;
+      case "town":
+        filter_function = (x) => x.town;
+        break;
+    }
+    if (by === "nameStartsWith") {
+      restaurant_list = restaurant_list.filter(
+        (x) => filter_function(x).indexOf(what) == 0
+      );
+      derivative_function = filter_function;
+      if (sort) {
+        restaurant_list.sort(derivative_compare);
+      }
+    } else {
+      restaurant_list = restaurant_list.filter(
+        (x) => filter_function(x).indexOf(what) != -1
+      );
+
+      if (sort) {
+        // sort globally by what we are filtering by
+        derivative_function = filter_function;
+        restaurant_list.sort(derivative_compare);
+
+        // sort so that things that match the search term earlier are earlier in the list
+        derivative_function = (x) => filter_function(x).indexOf(what);
+        restaurant_list.sort(derivative_compare);
+      }
+    }
+    return restaurant_list;
+  }
+
+  /*
+   * Overview: Input a list of restaurants and returns that list sorted by
+   *           desired criteria. Note: creates a copy of the data that you
+   *           are sorting, so the original data is retained.
+   *
+   * Params:
+   * restaurant_list: the list that you are filtering down
+   * by: A string describing what are you sorting by. Options are:
+   *      - "address": alphabetical ordering of the address of the restaurant
+   *      - "critical": number of critical violations
+   *      - "id": the establishment id of the restaurant
+   *      - "name": alphabetical order of the name
+   *      - "noncritical": the average number of non-critical violations per inspection
+   *      - "town": alphabetical order of the town
+   *      - "violations": the average number of violations per inspection
+   */
+  sorted_by(restaurant_list, by, ascending = true) {
+    restaurant_list = [...restaurant_list]; // clone the restaurant list
+    switch (by) {
+      case "address":
+        derivative_function = (x) => x.address;
+        break;
+      case "critical":
+        derivative_function = (x) =>
+          x.average_violations[1] + x.average_violations[2];
+        break;
+      case "id":
+        derivative_function = (x) => x.id;
+        break;
+      case "name":
+        derivative_function = (x) => x.name;
+        break;
+      case "noncritical":
+        derivative_function = (x) => x.average_violations[0];
+        break;
+      case "town":
+        derivative_function = (x) => x.town;
+        break;
+      case "violations":
+        derivative_function = (x) =>
+          x.average_violations[0] +
+          x.average_violations[1] +
+          x.average_violations[2];
+        break;
     }
 
-    /*
-    * Overview: Input a list of functions and some search parameters. It will return a list
-    *           filtered and (optionally) sorted based on the criteria. If you are searching
-    *           if the string contains a certain character, the sorting will favor restaurants
-    *           where that search term occurs earlier in the string. For example, if you are
-    *           searching for restaurants whose names contain "A", the first things in the list
-    *           will be restaurants whose name starts with "A", then has a second letter "A", etc.
-    *           Note that the list that is returned is a copy of the list. The original is retained.
-    *
-    * Params:
-    * restaurant_list: the list that you are filtering down
-    * by: A string describing what you would like to filter by. Options are:
-    *      - "address": does the address contain the given string
-    *      - "nameStartsWith": does the restaurant name start with the given string
-    *      - "nameContains": does the restaurant name contain the given string
-    *      - "town": does the town name contain the given string
-    * what: the string of the search query
-    * sort: whether or not the data should also be sorted by the thing that you are searching by
-    * 
-    */
-    filtered_by(restaurant_list, by, what, sort=true ){
-        let filter_function = null
-        switch(by){
-            case "address":
-                filter_function = (x) => x.address
-                break
-            case "nameStartsWith":
-            case "nameContains":
-                filter_function = (x) => x.name
-                break
-            case "town":
-                filter_function = (x) => x.town
-                break
-        }
-        if( by === "nameStartsWith" ){
-            restaurant_list = restaurant_list.filter( (x) => filter_function(x).indexOf(what) == 0 )
-            derivative_function = filter_function
-            if( sort ){
-                restaurant_list.sort( derivative_compare )
-            }
-        }
-        else{        
-            restaurant_list = restaurant_list.filter( (x) => filter_function(x).indexOf(what) != -1 )
-
-            if( sort ){
-                // sort globally by what we are filtering by
-                derivative_function = filter_function
-                restaurant_list.sort( derivative_compare )
-
-                // sort so that things that match the search term earlier are earlier in the list
-                derivative_function = (x) => filter_function(x).indexOf(what)            
-                restaurant_list.sort( derivative_compare )
-            }
-        }
-        return restaurant_list
+    if (ascending) {
+      restaurant_list.sort(derivative_compare);
+    } else {
+      restaurant_list.sort(reverse_derivative_compare);
     }
+    return restaurant_list;
+  }
 
-    /*
-    * Overview: Input a list of restaurants and returns that list sorted by
-    *           desired criteria. Note: creates a copy of the data that you
-    *           are sorting, so the original data is retained.
-    * 
-    * Params:
-    * restaurant_list: the list that you are filtering down
-    * by: A string describing what are you sorting by. Options are:
-    *      - "address": alphabetical ordering of the address of the restaurant
-    *      - "critical": number of critical violations
-    *      - "id": the establishment id of the restaurant
-    *      - "name": alphabetical order of the name
-    *      - "noncritical": the average number of non-critical violations per inspection
-    *      - "town": alphabetical order of the town
-    *      - "violations": the average number of violations per inspection
-    */
-    sorted_by(restaurant_list, by, ascending=true){
-        restaurant_list = [...restaurant_list] // clone the restaurant list    
-        switch(by){
-            case "address":
-                derivative_function = (x) => x.address
-                break
-            case "critical":
-                derivative_function = (x) => x.average_violations[1] + x.average_violations[2]
-                break
-            case "id":
-                derivative_function = (x) => x.id
-                break
-            case "name":
-                derivative_function = (x) => x.name
-                break
-            case "noncritical":
-                derivative_function = (x) => x.average_violations[0]
-                break
-            case "town":
-                derivative_function = (x) => x.town;
-                break
-            case "violations":
-                derivative_function = (x) => x.average_violations[0] + x.average_violations[1] + x.average_violations[2]
-                break
-        }
+  // ---------------------- HELPER FUNCTIONS ---------------------------
 
-        if(ascending){
-            restaurant_list.sort( derivative_compare )
-        }
-        else{
-            restaurant_list.sort( reverse_derivative_compare )
-        }
-        return restaurant_list
+  // a function that allows us compare functions by derived values
+  // the derived value that we are comparing by is stored in the function
+  // derivative_function
+  derivative_compare(x, y) {
+    let val1 = derivative_function(x);
+    let val2 = derivative_function(y);
+    if (val1 < val2) {
+      return -1;
     }
-
-    // ---------------------- HELPER FUNCTIONS ---------------------------
-
-    // a function that allows us compare functions by derived values
-    // the derived value that we are comparing by is stored in the function
-    // derivative_function
-    derivative_compare(x,y){
-        let val1 = derivative_function(x)
-        let val2 = derivative_function(y)
-        if(val1 < val2){
-            return -1
-        }
-        if(val1 > val2){
-            return 1
-        }
-        return 0
+    if (val1 > val2) {
+      return 1;
     }
+    return 0;
+  }
 
-    // allows us to sort by derivative compare,
-    // but in reverse
-    reverse_derivative_compare(x,y){
-        return -1*derivative_compare(x,y)
-    }
-
+  // allows us to sort by derivative compare,
+  // but in reverse
+  reverse_derivative_compare(x, y) {
+    return -1 * derivative_compare(x, y);
+  }
 }
-
-
-
-
--- js/script.js
+++ js/script.js
@@ -1,29 +1,88 @@
 /**
-* Requests the file and executes a callback with the parsed result once
-* it is available
-* @param {string} path - The path to the file.
-* @param {function} callback - The callback function to execute once the result is available
-*/
+ * Requests the file and executes a callback with the parsed result once
+ * it is available
+ * @param {string} path - The path to the file.
+ * @param {function} callback - The callback function to execute once the result is available
+ */
 
-function fetchJSONFile (path, callback) {
-    const httpRequest = new XMLHttpRequest();
-    httpRequest.onreadystatechange = function () {
-
-      if (httpRequest.readyState === 4) {
-        if (httpRequest.status === 200) {
-          const data = JSON.parse(httpRequest.responseText);
-          if (callback) callback(data);
-        }
+function fetchJSONFile(path, callback) {
+  const httpRequest = new XMLHttpRequest();
+  httpRequest.onreadystatechange = function () {
+    if (httpRequest.readyState === 4) {
+      if (httpRequest.status === 200) {
+        const data = JSON.parse(httpRequest.responseText);
+        if (callback) callback(data);
       }
-    };
-    httpRequest.open('GET', path);
-    httpRequest.send();
-  } 
-  
-  // call fetchJSONFile then build and render a tree
-  // this is the function executed as a callback when parsing is done
-fetchJSONFile('data/data_with_towns.json', function (data){
-    const proccessData = new ProcessData(data);
-    proccessData.process_data();
+    }
+  };
+  httpRequest.open("GET", path);
+  httpRequest.send();
+}
 
+// call fetchJSONFile then create ProcessData object to access globally
+// setup visualization
+var processData = fetchJSONFile("data/data_with_towns.json", function (data) {
+  const proccessData = new ProcessData(data);
+  proccessData.process_data();
+  setup(proccessData);
 });
+
+function setup(processdata) {
+  // add an event listener to all menu and selection elements
+  // update processData object accordingly in the changeData function
+  // ref: https://d3js.org/d3-selection/events
+  // ref: https://www.tutorialsteacher.com/d3js/select-dom-element-using-d3js
+  // ref: https://stackoverflow.com/questions/24193593/d3-how-to-change-dataset-based-on-drop-down-box-selection
+  filterby_listener = d3
+    .select("#filter-by")
+    .on("change", (event) => update(processdata, "filter"));
+  filterwhat_listener = d3
+    .select("#filter-what")
+    .on("change", (event) => update(processdata, "filter"));
+  filtersort_listener = d3
+    .select("#filter-sort")
+    .on("change", (event) => update(processdata, "filter"));
+
+  // update visualizations
+  update(processdata, "none");
+}
+
+/**
+ * Update the data according to document settings
+ * Param:
+ * data - the ProcessData object being used
+ * change - a string indicating what change is to occur
+ */
+function update(processdata, change) {
+  // to hold data to use (currently the processData object)
+  curr_data = processdata;
+  // call ProcessData function accordingly
+  if (change === "filter") {
+    // get filter parameters
+    filterby = d3.select("#filter-by").node().value;
+    filterwhat = d3.select("#filter-what").node().value;
+    filtersort = d3.select("#filter-sort").node().value;
+    if (filtersort === "on") {
+      filtersort = True;
+    } else {
+      filtersort = False;
+    }
+    curr_data = processdata.filtered_by(
+      processdata.restaurants,
+      filterby,
+      filterwhat,
+      filtersort
+    );
+  }
+
+  // update the charts and plots with the appropriate functions
+  updateBarChart();
+}
+
+/**
+ * Update the bar chart
+ */
+
+function updateBarChart() {
+  console.log("inside updateBarChart()");
+}
